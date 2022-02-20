abb381e315f60c558b98836b829d5c22e312c32e
TohaRodiv <toha.rodiv@yandex.ru>
2022-02-20 21:04:12 +1000

    Начальная интеграция авторизации

---
+++ src/components/organisms/auth-container/AuthContainer.tsx
@@ -0,0 +1,31 @@
+import { AuthLayout } from "#components/templates/auth-layout";
+import { UserService } from "#services/user-service";
+import { TAccessProps } from "#types/TAccessProps";
+import { FC, ReactNode, } from "react";
+
+type TProps = {
+	access: TAccessProps
+	children: ReactNode
+}
+
+
+export const AuthContainer: FC<TProps> = ({
+	access,
+	children,
+}): JSX.Element => {
+
+	UserService.setAuthorized(access.isAuthorized);
+
+	if (UserService.checkAuthorized()) {
+		return (
+			<>
+				{children}
+			</>
+		);
+	} else {
+		return (
+			<AuthLayout />
+		);
+	}
+
+};
